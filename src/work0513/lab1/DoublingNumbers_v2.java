c5c5d6386e613e9f3538604295a00d99fa51d777
ZhaoPengshuai <2542148593@qq.com>
2023-05-22 17:09:07 +0800

    delete some files

--- src/work0513/lab1/DoublingNumbers_v2.java
+++
@@ -1,7 +0,0 @@
-public class DoublingNumbers_v2 {
-    public static void main(String[] args) {
-       for(int i = 1; i <= 10; i++) {
-           System.out.println("The double of " + i + " is " + 2*i);
-       }
-}
-}
